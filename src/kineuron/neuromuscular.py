7cdaf88b11903ac902ffc1e9d306d2b3552de53a
Alejandro Martínez-Valencia <vaf.ss4@gmail.com>
2022-02-13 04:47:08 -0600

    Updated the source code directory location to src and added the setup files

--- src/kineuron/neuromuscular.py
+++ src/kineuron/neuromuscular.py
@@ -3,7 +3,9 @@ class Synapse:
         self.__name: str = name
 
     def get_name(self) -> str:
+        """Returns the user-defined name of the instantiated object."""
         return self.__name
 
     def get_info(self) -> None:
+        """Returns the general information of the object."""
         print("This is a element of synapse")
